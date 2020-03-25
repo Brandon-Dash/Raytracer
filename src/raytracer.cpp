8dfcbae95c671ce664d35e22aa3dbf860c5b6ed9
Brandon-Dash <brandon_dash@live.ca>
2020-03-24 19:06:18 -0500

    add ray-sphere intersection

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -21,8 +21,6 @@ json scene;
 
 /****************************************************************************/
 
-// here are some potentially useful utility functions
-
 json find(json &j, const std::string key, const std::string value) {
 	json::iterator it;
 	for (it = j.begin(); it != j.end(); ++it) {
@@ -37,6 +35,32 @@ json find(json &j, const std::string key, const std::string value) {
 
 glm::vec3 vector_to_vec3(const std::vector<float> &v) {
 	return glm::vec3(v[0], v[1], v[2]);
+}
+
+bool hitsphere(point3 d, point3 e, point3 c, float r, point3 &hit) {
+	double disc = pow(glm::dot(d, e - c), 2) - glm::dot(d, d) * (glm::dot(e - c, e - c) - r * r);
+
+	if (disc < 0)
+		return false;
+
+	double rest = glm::dot(-d, e - c) / glm::dot(d, d);
+	double sqrtdisc = sqrt(disc);
+	double t;
+
+	if (disc > 0) {
+		double t1 = rest + sqrtdisc / glm::dot(d, d);
+		double t2 = rest - sqrtdisc / glm::dot(d, d);
+		if (t1 < t2)
+			t = t1;
+		else
+			t = t2;
+	}
+	else {
+		t = rest;
+	}
+
+	hit = e + float(t) * d;
+	return true;
 }
 
 /****************************************************************************/
@@ -75,6 +99,9 @@ bool trace(const point3 &e, const point3 &s, colour3 &colour, bool pick) {
   // NOTE 2: You can work with JSON objects directly (like this sample code), read the JSON objects into your own data structures once and render from those (probably in choose_scene), or hard-code the objects in your own data structures and choose them by name in choose_scene; e.g. choose_scene('e') would pick the same scene as the one in "e.json". Your choice.
   // If you want to use this JSON library, https://github.com/nlohmann/json for more information. The code below gives examples of everything you should need: getting named values, iterating over arrays, and converting types.
 
+	// hardcoding a light. remove this in your implementation
+	point3 light(0.5, 10, 2);
+
 	// traverse the objects
 	json &objects = scene["objects"];
 	for (json::iterator it = objects.begin(); it != objects.end(); ++it) {
@@ -86,11 +113,30 @@ bool trace(const point3 &e, const point3 &s, colour3 &colour, bool pick) {
 			// Every sphere will have a position and a radius
 			std::vector<float> pos = object["position"];
 			point3 p = -(s - e) * pos[2];
-			if (glm::length(glm::vec3(p.x - pos[0],p.y - pos[1],0)) < float(object["radius"])) {
+
+			point3 c = vector_to_vec3(pos);
+			point3 d = s - e;
+			float r = float(object["radius"]);
+
+			point3 hitpos;
+			bool didhit;
+
+			didhit = hitsphere(d, e, c, r, hitpos);
+
+			if (didhit) {
+				if (pick)
+					std::cout << "hitpos = {" << hitpos[0] << ", " << hitpos[1] << ", " << hitpos[2] << "}" << std::endl;
+
 				// Every object will have a material
 				json &material = object["material"];
 				std::vector<float> diffuse = material["diffuse"];
 				colour = vector_to_vec3(diffuse);
+
+				point3 L = glm::normalize(light - hitpos);
+				point3 N = glm::normalize(hitpos - c);
+
+				colour = colour * glm::dot(N, L);
+
 				// This is NOT correct: it finds the first hit, not the closest
 				return true;
 			}
