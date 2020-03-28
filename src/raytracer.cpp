f3ae342b21d6e76c92bb446805be1c3516c11268
Brandon-Dash <brandon_dash@live.ca>
2020-03-28 03:31:23 -0500

    it doesn't work

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -47,7 +47,7 @@ glm::vec3 vector_to_vec3(const std::vector<float> &v) {
 
 // Ray intersection functions
 
-float hitsphere(point3 d, point3 e, point3 c, float r, point3 &hit) {
+float hitsphere(point3 d, point3 e, point3 c, float r, point3 &hit, bool exit = false) {
 	double disc = pow(glm::dot(d, e - c), 2) - glm::dot(d, d) * (glm::dot(e - c, e - c) - r * r);
 
 	if (disc < 0)
@@ -57,23 +57,31 @@ float hitsphere(point3 d, point3 e, point3 c, float r, point3 &hit) {
 	double sqrtdisc = sqrt(disc);
 	double t;
 
-	if (disc > 0) {
-		double t1 = rest + sqrtdisc / glm::dot(d, d);
-		double t2 = rest - sqrtdisc / glm::dot(d, d);
-		if (t1 < t2)
-			t = t1;
-		else
-			t = t2;
-	}
-	else {
-		t = rest;
-	}
+	if (exit)
+		t = rest + sqrtdisc / glm::dot(d, d);
+	else
+		t = rest - sqrtdisc / glm::dot(d, d);
+
+	if (t < 0)
+		return 0;
 
 	hit = e + float(t) * d;
 	return float(t);
 }
 
-float hitplane(point3 d, point3 e, point3 a, point3 n, point3 &hit) {
+bool pointInTriangle(const point3 &point, const point3 &p1, const point3 &p2, const point3 &p3, const point3 &n) {
+	float test1 = glm::dot(glm::cross(point - p1, p2 - p1), n);
+	float test2 = glm::dot(glm::cross(point - p2, p3 - p2), n);
+	float test3 = glm::dot(glm::cross(point - p3, p1 - p3), n);
+
+	return (test1 >= 0 && test2 >= 0 && test3 >= 0) || (test1 <= 0 && test2 <= 0 && test3 <= 0);
+}
+
+float hitplane(point3 d, point3 e, point3 a, point3 normal, point3 &hit, bool exit = false) {
+	point3 n = normal;
+	if (exit)
+		n = -normal;
+
 	double numerator = glm::dot(n, a - e);
 	double denominator = glm::dot(n, d);
 	double t = numerator / denominator;
@@ -87,12 +95,30 @@ float hitplane(point3 d, point3 e, point3 a, point3 n, point3 &hit) {
 	}
 }
 
-bool pointInTriangle(const point3 &point, const point3 &p1, const point3 &p2, const point3 &p3, const point3 &n) {
-	float test1 = glm::dot(glm::cross(point - p1, p2 - p1), n);
-	float test2 = glm::dot(glm::cross(point - p2, p3 - p2), n);
-	float test3 = glm::dot(glm::cross(point - p3, p1 - p3), n);
+float hitmesh(point3 d, point3 e, const json triangles, point3 &hit, point3 &hitNormal, bool exit = false) {
+	float t_min = MAX_T;
 
-	return (test1 >= 0 && test2 >= 0 && test3 >= 0) || (test1 <= 0 && test2 <= 0 && test3 <= 0);
+	for (int i = 0; i < triangles.size(); i++) {
+		std::vector<json> triangle = triangles[i];
+
+		point3 p1 = vector_to_vec3(triangle[0]);
+		point3 p2 = vector_to_vec3(triangle[1]);
+		point3 p3 = vector_to_vec3(triangle[2]);
+
+		point3 n = glm::cross(p2 - p1, p3 - p2);
+		point3 hitpos;
+
+		float t = hitplane(d, e, p1, n, hitpos, exit);
+
+		if (t > 0.0 && t < t_min && pointInTriangle(hitpos, p1, p2, p3, n)) {
+			hit = hitpos;
+			hitNormal = glm::normalize(n);
+			t_min = t;
+		}
+	}
+	if (t_min == MAX_T)
+		return 0.0;
+	return t_min;
 }
 
 /****************************************************************************/
@@ -192,8 +218,96 @@ bool shadowTest(const point3 &point, const point3 &lightPos, point3 &shadow) {
 	return true;
 }
 
-void light(const point3 &p, const point3 &V, const point3 &N, json &material, colour3 &colour, bool pick, int reflectionCount) {
+bool refractRay(const point3 &Vi, const point3 &N, float refractionRatio, point3 &Vr) {
+	float VidotN = glm::dot(Vi, N);
+
+	if (pow(VidotN, 2) < 1 - pow(1 / refractionRatio, 2))
+		return false;
+
+	Vr = refractionRatio * (Vi - N * VidotN) - N * sqrt(1 - pow(refractionRatio, 2) * (1 - pow(VidotN, 2)));
+	return true;
+}
+
+void reflectRay(const point3 &V, const point3 &N, point3 &R) {
+	R = glm::normalize(2 * (glm::dot(N, V)) * N - V);
+}
+
+bool calcTransRay(const point3 &inPoint, const point3 &inVector, const point3 &inNormal, const json &object, point3 &outPoint, point3 &outVector) {
+	json material = object["material"];
+	float refraction = 1.0;
+	point3 innerVector;
+
+	if (material.find("refraction") != material.end()) {
+		refraction = material["refraction"];
+		refractRay(inVector, inNormal, 1.0 / refraction, innerVector);
+	}
+	else
+		innerVector = inVector;
+
+	if (object["type"] == "sphere") {
+		if (refraction == 1.0) {
+			outVector = innerVector;
+			outPoint = inPoint + 1e-5f * outVector;
+		}
+		else {
+			point3 c = vector_to_vec3(object["position"]);
+			float r = float(object["radius"]);
+			point3 hitpos;
+
+			bool result = false;
+			int recursion = 0;
+			point3 currentPoint = inPoint;
+			while (!result && recursion < 8) {
+				hitsphere(innerVector, currentPoint, c, r, outPoint, true);
+				point3 outNormal = glm::normalize(c - hitpos);
+
+				result = refractRay(innerVector, outNormal, refraction, outVector);
+
+				if (!result) {
+					point3 R;
+					reflectRay(innerVector, outNormal, R);
+					innerVector = R;
+					currentPoint = outPoint;
+					recursion++;
+				}
+			}
+			if (!result)
+				return false;
+		}
+	}
+	else if (object["type"] == "mesh") {
+		std::vector<json> triangles = object["triangles"];
+		point3 hitpos, outNormal;
+
+		bool result = false;
+		int recursion = 0;
+		point3 currentPoint = inPoint;
+		while (!result && recursion < 8) {
+			hitmesh(innerVector, currentPoint, triangles, outPoint, outNormal, true);
+
+			result = refractRay(innerVector, -outNormal, refraction, outVector);
+
+			if (!result) {
+				point3 R;
+				reflectRay(-innerVector, -outNormal, R);
+				innerVector = R;
+				currentPoint = outPoint;
+				recursion++;
+			}
+		}
+		if(!result)
+			return false;
+	}
+	else if (object["type"] == "plane") {
+		outPoint = inPoint;
+		outVector = innerVector;
+	}
+	return true;
+}
+
+void light(const point3 &p, const point3 &V, const point3 &N, const json &object, colour3 &colour, bool pick, int reflectionCount) {
 	json &lights = scene["lights"];
+	json material = object["material"];
 
 	colour3 Ka, Kd, Ks, Kr, Kt = colour3(0.0, 0.0, 0.0);
 	float a = 0.0;
@@ -211,7 +325,8 @@ void light(const point3 &p, const point3 &V, const point3 &N, json &material, co
 
 	if (material.find("reflective") != material.end() && reflectionCount < MAX_REFLECTIONS) {
 		Kr = vector_to_vec3(material["reflective"]);
-		point3 R = glm::normalize(2 * (glm::dot(N, V)) * N - V);
+		point3 R;
+		reflectRay(V, N, R);
 		bool hit = trace(p + float(1e-5) * R, p + R, colour, pick, reflectionCount + 1);
 		if (!hit)
 			colour = background_colour;
@@ -280,7 +395,16 @@ void light(const point3 &p, const point3 &V, const point3 &N, json &material, co
 	if (material.find("transmissive") != material.end()) {
 		Kt = vector_to_vec3(material["transmissive"]);
 		colour3 transcolour = colour3(0.0, 0.0, 0.0);
-		bool hit = trace(p + float(1e-5) * -V, p + -V, transcolour, pick, reflectionCount);
+		point3 transmissionOrigin, transmissionDirection;
+
+		bool result = calcTransRay(p, -V, N, object, transmissionOrigin, transmissionDirection);
+
+		if (result) {
+			bool hit = trace(transmissionOrigin, transmissionOrigin + transmissionDirection, transcolour, pick, reflectionCount + 1);
+			if (!hit)
+				transcolour = background_colour;
+		}
+
 		colour = (colour3(1.0, 1.0, 1.0) - Kt) * colour + Kt * transcolour;
 	}
 }
@@ -321,7 +445,7 @@ bool trace(const point3 &e, const point3 &s, colour3 &colour, bool pick, int ref
   // NOTE 2: You can work with JSON objects directly (like this sample code), read the JSON objects into your own data structures once and render from those (probably in choose_scene), or hard-code the objects in your own data structures and choose them by name in choose_scene; e.g. choose_scene('e') would pick the same scene as the one in "e.json". Your choice.
   // If you want to use this JSON library, https://github.com/nlohmann/json for more information. The code below gives examples of everything you should need: getting named values, iterating over arrays, and converting types.
 
-	json material = NULL;
+	json hitObject = NULL;
 	point3 p, N, V;
 	float t_min = MAX_T;
 
@@ -341,11 +465,7 @@ bool trace(const point3 &e, const point3 &s, colour3 &colour, bool pick, int ref
 			float t = hitsphere(d, e, c, r, hitpos);
 
 			if (t > 0 && t < t_min) {
-				if (pick)
-					std::cout << "hitpos = {" << hitpos[0] << ", " << hitpos[1] << ", " << hitpos[2] << "}" << std::endl;
-
-				// Every object will have a material
-				material = object["material"];
+				hitObject = object;
 				p = hitpos;
 				N = glm::normalize(hitpos - c);
 				V = glm::normalize(e - hitpos);
@@ -361,10 +481,7 @@ bool trace(const point3 &e, const point3 &s, colour3 &colour, bool pick, int ref
 			float t = hitplane(d, e, a, n, hitpos);
 
 			if (t > 0 && t < t_min) {
-				if (pick)
-					std::cout << "hitpos = {" << hitpos[0] << ", " << hitpos[1] << ", " << hitpos[2] << "}" << std::endl;
-
-				material = object["material"];
+				hitObject = object;
 				p = hitpos;
 				N = glm::normalize(n);
 				V = glm::normalize(e - hitpos);
@@ -375,36 +492,32 @@ bool trace(const point3 &e, const point3 &s, colour3 &colour, bool pick, int ref
 			std::vector<json> triangles = object["triangles"];
 			
 			for (int i = 0; i < triangles.size(); i++) {
-				std::vector<json> triangle = triangles[i];
-
-				point3 p1 = vector_to_vec3(triangle[0]);
-				point3 p2 = vector_to_vec3(triangle[1]);
-				point3 p3 = vector_to_vec3(triangle[2]);
-
-				point3 n = glm::cross(p2 - p1, p3 - p2);
 				point3 d = s - e;
 				point3 hitpos;
+				point3 n;
 
-				float t = hitplane(d, e, p1, n, hitpos);
+				float t = hitmesh(d, e, triangles, hitpos, n);
 
 				if (t > 0 && t < t_min) {
-					if (pointInTriangle(hitpos, p1, p2, p3, n)) {
-						material = object["material"];
-						p = hitpos;
-						N = glm::normalize(n);
-						V = glm::normalize(e - hitpos);
-						t_min = t;
-					}
+					hitObject = object;
+					p = hitpos;
+					N = glm::normalize(n);
+					V = glm::normalize(e - hitpos);
+					t_min = t;
+					
 				}
 			}
 			
 		}
 	}
 
-	if (material == NULL)
+	if (hitObject == NULL)
 		return false;
 
-	light(p, V, N, material, colour, pick, reflectionCount);
+	if (pick)
+		std::cout << "hitpos = {" << p[0] << ", " << p[1] << ", " << p[2] << "}" << std::endl;
+
+	light(p, V, N, hitObject, colour, pick, reflectionCount);
 
 	return true;
 }
